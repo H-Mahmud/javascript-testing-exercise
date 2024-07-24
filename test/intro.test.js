53e16f55eb11955c3bd3629d13f047e82b30738f
Mahmud <me@imahmud.com>
2024-07-24 21:38:19 +0600

    Complete fizzBuzz exercise unit test

--- test/intro.test.js
+++ test/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { max } from "../src/intro";
+import { fizzBuzz, max } from "../src/intro";
 
 describe("max", () => {
     it("should return the first argument if it is getter", () => {
@@ -12,5 +12,23 @@ describe("max", () => {
 
     it("should return the first argument if arguments are equal", () => {
         expect(max(1, 1)).toBe(1);
+    });
+});
+
+describe("fizzBuzz", () => {
+    it("should return FizzBuzz if argument is devisable by 3 and 5", () => {
+        expect(fizzBuzz(15)).toBe("FizzBuzz");
+    });
+
+    it("should return Fizz if argument is only devisable by 3", () => {
+        expect(fizzBuzz(6)).toBe("Fizz");
+    });
+
+    it("should return Buzz if argument is only devisable by 5", () => {
+        expect(fizzBuzz(25)).toBe("Buzz");
+    });
+
+    it("should return argument string if it is not devisable by 3 or 5", () => {
+        expect(fizzBuzz("1")).toBe("1");
     });
 });
