33812ff2eb7302117b5891f01998cd83f3ce6f4c
Mahmud <me@imahmud.com>
2025-03-21 08:02:53 +0600

    Test getShippingInfo function

--- test/mocking.test.js
+++ test/mocking.test.js
@@ -1,8 +1,10 @@
 import { it, vi, expect, describe } from "vitest";
-import { getPriceInCurrency } from "../src/mocking";
+import { getPriceInCurrency, getShippingInfo } from "../src/mocking";
 import { getExchangeRate } from "../src/libs/currency";
+import { getShippingQuote } from "../src/libs/shipping";
 
 vi.mock("../src/libs/currency");
+vi.mock("../src/libs/shipping");
 
 describe("getPriceInCurrency", () => {
     it("should return price in target currency", () => {
@@ -11,5 +13,27 @@ describe("getPriceInCurrency", () => {
         const result = getPriceInCurrency(1.5, "USD");
 
         expect(result).toBe(15);
+    });
+});
+
+describe("getShippingInfo", () => {
+    it("should return shipping unavailable if quote cannot be fetched", () => {
+        vi.mocked(getShippingQuote).mockReturnValue(null);
+
+        const result = getShippingInfo("Dhaka");
+
+        expect(result).toMatch(/unavailable/i);
+    });
+
+    it("should return shipping info if quote can be fetched", () => {
+        vi.mocked(getShippingQuote).mockReturnValue({
+            cost: 10,
+            estimatedDays: 3,
+        });
+
+        const result = getShippingInfo("Dhaka");
+
+        expect(result).toMatch("$10");
+        expect(result).toMatch(/3 days/i);
     });
 });
